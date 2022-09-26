db44d5c656b51b6126f2081152093b4e9e2530de
Harrisoned3 <105175216+Harrisoned3@users.noreply.github.com>
2022-09-26 16:02:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var mouseEvent = "empty"
+document.getElementById("myCanvas");
+ctx = myCanvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.linewidth = 1;
+ctx.rect(150,143,430,200)
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.linewidth = 10;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.linewidth = 10;
+ctx.arc(290,250,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.linewidth = 10;
+ctx.arc(340,210,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.linewidth = 10;
+ctx.arc(390,250,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.linewidth = 10;
+ctx.arc(430,210,40,0,2*Math.PI);
+ctx.stroke();
